2d29e5d0acbf50786afee7ba4fff8324e620c69b
Will Flores <waflores956@gmail.com>
2026-05-13 19:43:37 -0400

    Updating the .gitignore to not include the .platformio folder. We also leverage the teensy-loader-cli to program the teensy and we update the devshell so that platformio will work nicely.

--- devshell.nix
+++ devshell.nix
@@ -3,21 +3,29 @@
   pkgs ? import <nixpkgs> { },
   ...
 }:
+let
+  teensy_loader_cli = pkgs.teensy-loader-cli.overrideAttrs (old: rec {
+    installPhase = old.installPhase + ''
+      ln -sf $out/bin/teensy-loader-cli $out/bin/teensy_loader_cli
+    '';
+  });
+in
 pkgs.mkShell {
   name = "tools";
-  packages = with pkgs; [
-    # keep-sorted start
-
-    avrdude
-    bashInteractive
-    nix-fast-build
-    nix-output-monitor
-    nix-tree
-    platformio
-    teensy-loader-cli
-    tree
-    # keep-sorted end
-  ];
+  packages =
+    with pkgs;
+    [
+      # keep-sorted start
+      avrdude
+      bashInteractive
+      nix-fast-build
+      nix-output-monitor
+      nix-tree
+      platformio
+      tree
+      # keep-sorted end
+    ]
+    ++ [ teensy_loader_cli ];
   shellHook = ''
     export PLATFORMIO_CORE_DIR=$PWD/.platformio
   '';
