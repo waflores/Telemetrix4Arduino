12d2861eae81b8acbf5b335b1c3df72d471f1559
Will Flores <waflores956@gmail.com>
2026-05-18 18:47:27 -0400

    Updating the flake.nix to the latest and greatest data.

--- flake.nix
+++ flake.nix
@@ -6,12 +6,13 @@
     # keep-sorted start
 
     blueprint.inputs.nixpkgs.follows = "nixpkgs";
-    blueprint.url = "https://github.com/numtide/blueprint/archive/56131e8628f173d24a27f6d27c0215eff57e40dd.tar.gz";
+    blueprint.url = "https://github.com/numtide/blueprint/archive/56131e8628f173d24a27f6d27c0215eff57e40dd.tar.gz"; # main - 2026-05-17
     devshell.inputs.nixpkgs.follows = "nixpkgs";
     devshell.url = "https://github.com/numtide/devshell/archive/255a2b1725a20d060f566e4755dbf571bbbb5f76.tar.gz";
-    nixpkgs.url = "https://github.com/NixOS/nixpkgs/archive/7f817b8455a1e9a944ab4392cc16c3ca36dbc83e.tar.gz"; # master - 2026-04-17
+    nixpkgs.url = "https://github.com/NixOS/nixpkgs/archive/64e79a178cb8eab2485e676ec40195d1818b5c09.tar.gz"; # master - 2026-05-17
     treefmt-nix.inputs.nixpkgs.follows = "nixpkgs";
-    treefmt-nix.url = "https://github.com/numtide/treefmt-nix/archive/790751ff7fd3801feeaf96d7dc416a8d581265ba.tar.gz";
+    treefmt-nix.url = "https://github.com/numtide/treefmt-nix/archive/790751ff7fd3801feeaf96d7dc416a8d581265ba.tar.gz"; # main - 2026-05-17
+
     # keep-sorted end
   }; # End inputs
 
